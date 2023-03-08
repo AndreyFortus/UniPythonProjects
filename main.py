ee77f9e2a781a3f263e73e2a0d17b2468a643c33
Andrey <andrey020904@gmail.com>
2023-03-08 19:36:54 +0200

    code improve

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import math
-#comment example
-#new test comment
-print("""Мультипарадигмальні мови програмування, Lab№1
-Андрій Фортус, ІКМ-221а, номер в списку 20""")
-for i in range(0,46):
-    if i < 45:
-        print('Владислав Валерійович Овсяніков, ', end='')
-    if i == 45:
-        print('Владислав Валерійович Овсяніков')
+# Info about the subject and the student
+print("""Multi-paradigm programming languages, Task №1
+Andrey Fortus, IKM-221a, №20""")
 
-print((pow(12,2)+17.1)*(73-254.2)/(17.4-211.3)-4.2)
+# Info about the educator
+EDUCATOR_NAME = "Vladyslav Valerievich Ovsyanikov"
+name_list = [EDUCATOR_NAME for i in range(45)]
+print(name_list)
+
+# Math example
+print((pow(12, 2) + 17.1) * (73 - 254.2) / (17.4 - 211.3) - 4.2)
