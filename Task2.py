dd29b72c0f6a4fd1720768030c6c08ccd6a1025e
Andrey <andrey020904@gmail.com>
2023-03-13 09:27:59 +0200

    add template and fix input error

--- Task2.py
+++ Task2.py
@@ -3,17 +3,22 @@
 
 GENERAL_INFO = 'Multi-paradigm programming languages, Task №2'
 STUDENT_INFO = 'Andrey Fortus IKM-221a'
+TEMPLATE = 'Input {} value: '
 
 # info
 print(GENERAL_INFO)
 print(STUDENT_INFO)
 
 # Math
-x = float(input('Enter x value: '))
-y = float(input('Enter y value: '))
-z = float(input('Enter z value: '))
 try:
-    result = (pow(12, x) + 17.1) * (y - 254.2) / (17.4 - z) - 4.2
-    print(f'Result = {result}')
-except ZeroDivisionError:
-    print('Division by zero ! Try another value')
+    x = float(input(TEMPLATE.format('x')))
+    y = float(input(TEMPLATE.format('y')))
+    z = float(input(TEMPLATE.format('z')))
+except ValueError:
+    print('Value error! Try again!')
+else:
+    try:
+        result = (pow(12, x) + 17.1) * (y - 254.2) / (17.4 - z) - 4.2
+        print(f'Result = {result}')
+    except ZeroDivisionError:
+        print('Division by zero ! Try another value')
