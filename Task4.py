eb48d90ded34180002bda5bb9db72979ebe8a899
Andrey <andrey020904@gmail.com>
2023-03-15 21:39:39 +0200

    rewrite code without 2 'else'

--- Task4.py
+++ Task4.py
@@ -15,15 +15,14 @@ def fibonacci(n):
         return 0
     elif n < 3:
         return 1
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
+    return fibonacci(n-1) + fibonacci(n-2)
 
 
 # print n is the number of Fibonacci numbers
 n = int(input('Input number of Fibonacci numbers: '))
 if n <= 0:
     raise Exception('n must be more then 0')
-elif n > 0:
-    print('Fibonacci numbers: ')
-    fib_list = [fibonacci(i) for i in range(n)]
-    print(fib_list)
+
+print('Fibonacci numbers: ')
+fib_list = [fibonacci(i) for i in range(n)]
+print(fib_list)
