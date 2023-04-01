14887aa81acb0061d57d3a943dc42c8f69f7d78b
Andrey <andrey020904@gmail.com>
2023-04-01 13:50:14 +0300

    add task 6

---
+++ task_6.py
@@ -0,0 +1,16 @@
+
+# task 1
+total = 0
+with open('C:/Users/Andrii/Desktop/numbers.txt', 'r') as inp, open('C:/Users/Andrii/Desktop/sum_numbers.txt', 'w') as outp:
+    for line in inp:
+        try:
+            num = float(line)
+            total += num
+
+        except ValueError:
+            print('{} is not a number!'.format(line))
+    outp.write(format(total))
+    print('Total of all numbers: {}'.format(total))
+
+# task 2
+number = int(input)
