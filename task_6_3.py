bae886444cb3fbe8425da11a315fc68e3fa614e2
Andrey <andrey020904@gmail.com>
2023-04-01 21:44:52 +0300

    add task 6 (1 - 5)

---
+++ task_6_3.py
@@ -0,0 +1,6 @@
+list_str = []
+with open('C:/Users/Andrii/Desktop/learning_python.txt', 'r') as lstr:
+    for line in lstr:
+        line = line.strip()
+        list_str.append(lstr.readlines())
+print(list_str)
---
+++ task_6_5.py
@@ -0,0 +1,9 @@
+with open('C:/Users/Andrii/Desktop/guest_book.txt', 'w') as guest_b:
+    while True:
+        username = input('(Input \'exit\' to stop program) Input your name:')
+        if username == 'exit':
+            print('Thx for use our program!')
+            break
+        welcome_user = f'Welcome {username}!\n'
+        print(welcome_user)
+        guest_b.write(welcome_user)
