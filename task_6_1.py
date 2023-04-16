b76554c22a56eaadf69d622c71f08e4ac91823bc
Andrey <andrey020904@gmail.com>
2023-04-16 18:54:43 +0300

    task 6_1 fix f-string error

--- task_6_1.py
+++ task_6_1.py
@@ -9,4 +9,4 @@ with open('C:/Users/Andrii/Desktop/numbers.txt', 'r', encoding='utf-8') as inp,\
         except ValueError:
             print(f'{line.format()} not a number!')
     outp.write(format(TOTAL))
-    print('Total of all numbers: {}'.format(TOTAL))
+    print(f'Total of all numbers: {TOTAL}')
