0fa5eee0733022de90d3ff022a586a0433730587
Andrey <andrey020904@gmail.com>
2023-05-02 11:17:50 +0300

    add back files

---
+++ task_1.py
@@ -0,0 +1,12 @@
+EDUCATOR_NAME = 'Vladyslav Valerievich Ovsyanikov'
+
+# Info about the subject and the student
+print("""Multi-paradigm programming languages, Task №1
+Andrey Fortus, IKM-221a, №20""")
+
+# Info about the educator
+name_list = [EDUCATOR_NAME for i in range(45)]
+print(name_list)
+
+# Math example
+print((pow(12, 2) + 17.1) * (73 - 254.2) / (17.4 - 211.3) - 4.2)
