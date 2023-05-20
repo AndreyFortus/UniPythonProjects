a23b168298e8fe416299c64ee786e9127907fb58
Andrey <andrey020904@gmail.com>
2023-05-20 09:14:55 +0300

    add task 11 var 1, 2

---
+++ lab_11/task_11_var_1.py
@@ -0,0 +1,29 @@
+import re
+
+
+def replace_date(match):
+
+    date = match.group()
+    parts = re.split('[./]', date)
+
+    if len(parts[0]) == 4:
+        new_date = parts[2] + '.' + parts[1] + '.' + parts[0]
+    else:
+        new_date = parts[0].zfill(2) + '.' + parts[1].zfill(2) + '.' + parts[2].zfill(4)
+
+    return new_date
+
+
+def main():
+    with open('C:/Users/Andrii/Desktop/lab_11/date.txt', 'r', encoding='utf-8') as file_date:
+        text = file_date.read()
+
+    pattern = r'\b((0?[1-9]|[12]\d|3[01])[./](0?[1-9]|1[012])[./](\d{4}))|((\d{4})[./](0?[1-9]|1[012])[./](0?[1-9]|[12]\d|3[01]))\b'
+
+    new_text = re.sub(pattern, replace_date, text)
+
+    with open('C:/Users/Andrii/Desktop/lab_11/sorted_date.txt', 'w', encoding='utf-8') as file_sort:
+        file_sort.write(new_text)
+
+
+main()
---
+++ lab_11/task__11_var__2.py
@@ -0,0 +1,15 @@
+import re
+def separate_text():
+    with open('C:/Users/Andrii/Desktop/lab_11/text_example.txt', 'r', encoding='utf-8') as file:
+        text = file.read()
+    pattern = r'(?<=[.!?])\s+'
+    return re.split(pattern, text)
+
+def main():
+    i = 1
+    for sentence in separate_text():
+        print(f'Sentence {i}: {sentence}')
+        i += 1
+
+
+main()
