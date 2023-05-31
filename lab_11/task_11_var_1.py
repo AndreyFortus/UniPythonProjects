19ac3822fb074d4a18b457e135fde9be64654f90
Andrey <andrey020904@gmail.com>
2023-05-31 09:53:58 +0300

    little fix

--- lab_11/task_11_var_1.py
+++ lab_11/task_11_var_1.py
@@ -4,25 +4,20 @@ from constants import PATH
 
 
 def replace_date(match):
-
     date = match.group()
     parts = re.split('[./]', date)
 
     if len(parts[0]) == 4:
-        new_date = parts[2] + '.' + parts[1] + '.' + parts[0]
-    else:
-        new_date = parts[0].zfill(2) + '.' + parts[1].zfill(2) + '.' + parts[2].zfill(4)
-
-    return new_date
+        return parts[2] + '.' + parts[1] + '.' + parts[0]
+    return parts[0].zfill(2) + '.' + parts[1].zfill(2) + '.' + parts[2].zfill(4)
 
 
 def main():
     with open(PATH + 'date.txt', 'r', encoding='utf-8') as file_date:
         text = file_date.read()
 
-    pattern = r'\b((0?[1-9]|[12]\d|3[01])[./]' \
-              r'(0?[1-9]|1[012])[./](\d{4}))|((\d{4})[./](' \
-              r'0?[1-9]|1[012])[./](0?[1-9]|[12]\d|3[01]))\b'
+    pattern = r'\b((0?[1-9]|[12]\d|3[01])[./](0?[1-9]|1[012])[./](\d{4}))' \
+              r'|((\d{4})[./](0?[1-9]|1[012])[./](0?[1-9]|[12]\d|3[01]))\b'
 
     new_text = re.sub(pattern, replace_date, text)
 
